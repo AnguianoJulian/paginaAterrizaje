546281afd9b56f2942b1cc646066b9b358097527
julian257 <julian.anguiano23b@udgvirtual.udg.mx>
2024-04-16 14:19:56 -0600

    Actividad 3. Tercera fase de desarrollo, revisión y ajuste de los Sprints y el backlog

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ import teni from './assets/img/teni2.avif';
 function App() {
   return (
     <div class="body">
-        <img class="logo" src={logo} />;
+          <header>
+        <img class="logo" src={logo} alt=""/>
+    </header>
+    <div class="pagina">
+        <div class="producto-img">
+            <div class="imagen"><img src={teni} alt=""/></div>
+        </div>
+        <div class="producto">
+            <h2 class="producto-nombre">
+                TENIS DE RUNNING 4DFWD 3
+            </h2>
+            <h4 class="producto-precio">
+                $5,499
+            </h4>
+            <div class="producto-detalle">
+                <h3>
+                    UNISEX, HORMA GRANDE
+                </h3>
+                <p>
+                    Para pies delgados recomendamos comprar la talla inferior.
+                </p>
+            </div>
+            <div class="producto-compra">
+                <a href="https://www.adidas.mx/tenis-de-running-4dfwd-3/ID0853.html">Comprar</a>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
