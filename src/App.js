7c4b76e3bb54dfb8b2f4288dcb1beaeb3331f1ef
julian257 <julian.anguiano23b@udgvirtual.udg.mx>
2024-05-09 13:12:26 -0600

    Actividad 2. Reunión para revisión de modificaciones

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import logo from './assets/img/logo2.jpg';
 import teni from './assets/img/teni2.avif';
+import teni1 from './assets/img/teni1.avif';
+import teni3 from './assets/img/teni3.avif';
+import teni4 from './assets/img/tenis4.avif';
 
 function App() {
   return (
     <div class="body">
           <header>
-        <img class="logo" src={logo} alt=""/>
+          <a href="https://www.adidas.mx/">
+          <img class="logo" src={logo} alt=""/>
+          </a>
     </header>
     <div class="pagina">
         <div class="producto-img">
-            <div class="imagen"><img src={teni} alt=""/></div>
+            <div class="imagen"><img src={teni} alt="" height="500px"  width="500px"/></div>
+            <div class="imagen"><img src={teni1} alt="" height="500px"  width="500px"/></div>
+            <div class="imagen"><img src={teni3} alt="" height="500px"  width="500px"/></div>
+            <div class="imagen"><img src={teni4} alt="" height="500px"  width="500px"/></div>
+            
         </div>
         <div class="producto">
             <h2 class="producto-nombre">
@@ -32,6 +41,18 @@ function App() {
             </div>
         </div>
     </div>
+    <footer>
+        <nav class="lista">
+            <ul class="lista-contenido">
+                <li class="contenido">Configuración de las cookies</li>
+                <li class="contenido">Nuestros Datos</li>
+                <li class="contenido">Términos y Condiciones</li>
+                <li class="contenido">Uso del Sitio</li>
+                <li class="contenido">Términos y Condiciones Promociones</li>
+                <li class="contenido">Privacidad de Videovigilancia</li>
+            </ul>
+        </nav>
+    </footer>
     </div>
   );
 }
